81d270b0a378e1b600d9d0581efc749e62d7a5c5
Vcyprus32@mail.ru <Vcyprus32@mail.ru>
2022-12-27 21:31:31 +0200

    Добавьте файлы проекта.

---
+++ 9. Mass -1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Дан массив, убрать все повторяющиеся элементы, т.е. вывод на -1 
+
+int Count;
+int a;
+int b;
+int c;
+
+Random random = new Random();
+
+Console.Write("$ Введите любое число от 5 до 15 ");
+Count = int.Parse(Console.ReadLine());
+
+int[] mas = new int[Count];
+mas[Count] = random.Next(1, 5 + 1);
+
+Console.WriteLine($"Массив который срандомился : {mas}");
+
+    for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(1, 5 + 1);
+    a = mas[i];
+
+    for (b = i + 1; b < mas.Length; b++)   // тут скорее не b , потому что b в данном случае числовое значение индекса
+        if (mas[b] == a)
+        {
+            mas[b] = -1;
+        }
+    Console.WriteLine($"{mas.Length}");
+}
+
+
+
+
+Console.ReadKey();
+
